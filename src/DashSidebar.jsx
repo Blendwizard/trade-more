1efa17f66bdb898e3151449a1773ea1d5450b614
JoeVanCamp <vancampjoe@gmail.com>
2022-10-06 15:09:30 -0700

    updated navbar styling, removed menu tab component

--- src/DashSidebar.jsx
+++ src/DashSidebar.jsx
@@ -4,15 +4,20 @@ import MenuTab from "./ui_components/MenuTab";
 import NavigationGroup from "./NavigationGroup.jsx";
 
 const SideContainer = styled.div`
-display: flex;
-flex-direction: column;
-flex-wrap: wrap;
-align-content: center;
-justify-content: flex-start;
-width: 15%;
-height: auto;
-background: #2E2A48;
-border-radius: 49px 49px 0px 0px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: flex-start;
+  width: 15%;
+  height: auto;
+  background: rgba(4, 0, 31, 1);
+  border-radius: 25px;
+  border: 1px solid #c4a7eb6b;
+
+  padding-left: 1em;
+  padding-top: 3em;
+  padding-right: 1em;
 `;
 
 
@@ -21,7 +26,6 @@ const DashSidebar = ({changeView}) => {
   return (
     <>
     <SideContainer>
-
       <NavigationGroup changeView={changeView} />
     </SideContainer>
     </>
--- src/NavigationGroup.jsx
+++ src/NavigationGroup.jsx
@@ -3,17 +3,13 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import FlexContainer from "./ui_components/FlexContainer";
 
-const MainContainer = styled(FlexContainer)`
-
-  justify-content: center;
-`;
-
 const LinkGroup = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-
+  gap: 1em;
+  width: -webkit-fill-available;
 `;
 
 const LinkItem = styled.div`
@@ -21,39 +17,45 @@ const LinkItem = styled.div`
   flex-direction: row;
   align-items: flex-end;
   width: 100%;
-  margin-bottom: 25%
+  gap: 0.3em;
+  padding: 5px;
+  border-radius: 10px;
+  min-height: 2em;
+  &:hover {
+    background-color: #6a4feb3d;
+  }
 `;
+
+
 
 const NavigationGroup = ( {changeView} ) => {
 
 
   return (
-    <MainContainer>
     <LinkGroup>
+        <LinkItem>
+          <i className="ci-home_alt_fill" style={{"fontSize":"1.5em"}}></i>
+          <Link className="link" to="/">Home</Link>
+        </LinkItem>
       <LinkItem>
-        <i className="ci-home_alt_fill" style={{"fontSize":"1.5em", "marginRight": "7%"}}></i>
-        <Link className="link" to="/">Home</Link>
-      </LinkItem>
-      <LinkItem>
-        <i className="ci-line_chart_up" style={{"fontSize":"1.5em", "marginRight": "7%"}}></i>
+        <i className="ci-line_chart_up" style={{"fontSize":"1.5em"}}></i>
         <span onClick={changeView} className="link">Dashboard</span>
       </LinkItem>
 
       <LinkItem>
-        <i className="ci-search" style={{"fontSize":"1.5em", "marginRight": "7%"}}></i>
+        <i className="ci-search" style={{"fontSize":"1.5em"}}></i>
         <span onClick={changeView} className="link">Research</span>
       </LinkItem>
       <LinkItem>
 
-        <i className="ci-data" style={{"fontSize":"1.5em", "marginRight": "7%"}}></i>
+        <i className="ci-data" style={{"fontSize":"1.5em"}}></i>
         <Link className="link" to="/analytics">Analytics</Link>
       </LinkItem>
       <LinkItem>
-        <i className="ci-credit_card" style={{"fontSize":"1.5em", "marginRight": "7%"}}></i>
+        <i className="ci-credit_card" style={{"fontSize":"1.5em"}}></i>
         <Link className="link" to="/account">Funding</Link>
       </LinkItem>
     </LinkGroup>
-    </MainContainer>
   )
 
 };
